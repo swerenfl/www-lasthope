ec65188411171089be1713b4fa29c76c6b060815
Richard Staehler III <richard.staehler@zoro.com>
2020-09-11 23:14:02 -0500

    contact form

---
+++ assets/js/contact.js
@@ -0,0 +1,17 @@
+function sendMessage() {
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://discordapp.com/api/webhooks/753867196190556170/FtWnSZvS-3hXzzC7qwlXoA9JvmqW_TmkBRQkIWRK8dZEAdGPs61Igk_i9IltCaKVaIct");
+
+    request.setRequestHeader('Content-type', 'application/json');
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var params = {
+        username: "User Test",
+        avatar_url: "",
+        content: "From: " + name + ", Message:" + message
+    }
+
+    request.send(JSON.stringify(params));
+}
