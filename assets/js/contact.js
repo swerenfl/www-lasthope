f8ee2f678bdcdcd03670beb37eca3c8d2877806e
Richard Staehler III <richard.staehler@zoro.com>
2020-09-12 12:30:22 -0500

    contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,8 @@
 function sendMessage() {
-
     // Construct POSTS
     var request = new XMLHttpRequest();
     request.open("POST", "https://discordapp.com/api/webhooks/753867196190556170/FtWnSZvS-3hXzzC7qwlXoA9JvmqW_TmkBRQkIWRK8dZEAdGPs61Igk_i9IltCaKVaIct");
-
+    
     // Obtain values from document
     request.setRequestHeader('Content-type', 'application/json');
     var charname = document.getElementById("charname").value;
@@ -13,45 +12,45 @@ function sendMessage() {
     var logs = document.getElementById("logs").value;
     var raiderio = document.getElementById("raiderio").value;
     var message = document.getElementById("message").value;
-
-    validateFields(charname, battletag, classtype, spec, logs, raiderio, message);
-
-    // Construct params
-    var params = {
-        username: "Application Received from: " + charname,
-        avatar_url: "",
-        content: "***Character Name:*** " + charname + 
-                    "\n***BattleTag:*** " + battletag +
-                    "\n***Class:*** " + classtype +
-                    "\n***Spec:*** " + spec +
-                    "\n***WCL:*** " + logs +
-                    "\n***raider.io:*** " + raiderio +
-                    "\n***What attracted you:*** " + message
-    }
-
-    // Send using JSON
-    request.send(JSON.stringify(params));
-
-    // Alert user that they submitted
-    var sideNav = document.getElementById("doneMsg");
-    sideNav.classList.toggle("hiddenContact");
-    sideNav.classList.toggle("visibleContact");
-
-    // Clear form after so many seconds
-    setTimeout(function(){
+    var validateFields = validateFields(charname, battletag, classtype, spec, logs, raiderio, message);
+    
+    // Check if blank then construct params
+    if (validateFields) {
+        var params = {
+            username: "Application Received from: " + charname,
+            avatar_url: "",
+            content: "***Character Name:*** " + charname +
+                "\n***BattleTag:*** " + battletag +
+                "\n***Class:*** " + classtype +
+                "\n***Spec:*** " + spec +
+                "\n***WCL:*** " + logs +
+                "\n***raider.io:*** " + raiderio +
+                "\n***What attracted you:*** " + message
+        }
+        
+        // Send using JSON
+        request.send(JSON.stringify(params));
+        
+        // Alert user that they submitted
+        var sideNav = document.getElementById("doneMsg");
         sideNav.classList.toggle("hiddenContact");
-        document.getElementById("charname").value = '';
-        document.getElementById("battletag").value = '';
-        document.getElementById("classtype").value = '';
-        document.getElementById("spec").value = '';
-        document.getElementById("logs").value = '';
-        document.getElementById("raiderio").value = '';
-        document.getElementById("message").value = '';
-    }, 3000);
+        sideNav.classList.toggle("visibleContact");
+        
+        // Clear form after so many seconds
+        setTimeout(function() {
+            sideNav.classList.toggle("hiddenContact");
+            document.getElementById("charname").value = '';
+            document.getElementById("battletag").value = '';
+            document.getElementById("classtype").value = '';
+            document.getElementById("spec").value = '';
+            document.getElementById("logs").value = '';
+            document.getElementById("raiderio").value = '';
+            document.getElementById("message").value = '';
+        }, 3000);
+    }
 }
 
 function validateFields(charname, battletag, classtype, spec, logs, raiderio, message) {
-
     if (charname == "") {
         alert("You forgot to fill in your Character Name. Please correct before submitting.");
         document.getElementById("charname").style.background = '#c59393';
@@ -87,4 +86,5 @@ function validateFields(charname, battletag, classtype, spec, logs, raiderio, me
         document.getElementById("message").style.background = '#c59393';
         return false;
     }
+    return true;
 }
