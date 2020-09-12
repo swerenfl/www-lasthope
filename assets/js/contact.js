0be9a9a9791f2e59339b2292f578883284bf99d2
Richard Staehler III <richard.staehler@zoro.com>
2020-09-12 15:25:13 -0500

    embeds

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -37,7 +37,9 @@ function sendMessage() {
                       "fields": [
                         {
                           "name": "Advanced formatting",
-                          "value": "Character Name\n> Hello.\n\nClass\n> Hello.\n\nSpec\n> Hello.\n\nLogs\n> Hello.\n\nraider.io\n> Hello.\n\nSummary\n> Hello."
+                          "value": "Character Name\n>" + charname + " \
+                                    \n\nClass\n>" + classtype + " \
+                                    \n\nSpec\n> Hello.\n\nLogs\n> Hello.\n\nraider.io\n> Hello.\n\nSummary\n> Hello."
                         }
                       ]
                     }
